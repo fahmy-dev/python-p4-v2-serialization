837aa02a8cbe98459a4933417fe40112893aca8a
fahmy-dev <faahmy@proton.me>
2025-03-29 17:36:59 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -20,6 +20,32 @@ def index():
     body = {'message': 'Welcome to the pet directory!'}
     return make_response(body, 200)
 
+@app.route('/pets/<int:id>')
+def get_pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        body = pet.to_dict()
+        status = 200
+    else:
+        body = {'error': f'Pet {id} not found'}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = Pet.query.filter(Pet.species == species).all()
+
+    if pets:
+        pets_list = [pet.to_dict() for pet in pets]
+        body = {'count': len(pets_list), 'pets': pets_list}
+        status = 200
+    else:
+        body = {'error': f'Species {species} does not exist'}
+        status = 404
+    
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
